5d40bdc0b29acef113a8f42abc20ad94a143009a
DanKatzuv <dan.dan.kafe@gmail.com>
2017-11-17 13:43:23 +0200

    Added motors
    Added the four robot driving motors.

--- src/org/usfirst/frc/team5987/robot/RobotMap.java
+++ src/org/usfirst/frc/team5987/robot/RobotMap.java
@@ -10,7 +10,10 @@ public class RobotMap {
     // following variables to use with your drivetrain subsystem.
     // public static int leftMotor = 1;
     // public static int rightMotor = 2;
-    
+    public static int leftFrontMotor = 1;
+    public static int leftRearMotor = 2;
+    public static int rightFrontMotor = 3;
+    public static int rightRearMotor = 4;
     // If you are using multiple modules, make sure to define both the port
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
