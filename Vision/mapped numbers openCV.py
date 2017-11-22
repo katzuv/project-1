e0ea4e5b3169907a82291461f8a248b8ba6d1617
A Cool Name <acoolname9001@gmail.com>
2017-11-22 17:57:22 +0200

    added some documentations
    for the numbers file

--- Vision/mapped numbers openCV.py
+++ Vision/mapped numbers openCV.py
@@ -1,14 +1,20 @@
 import cv2
 import numpy as np
-grid=np.zeros((1000,1000,3),dtype=np.uint8)
-number=0
-base=36
+grid=np.zeros((1000,1000,3),dtype=np.uint8) # an empty grid to test on
+number=0 #base number for later calculations
+base=36 #doron and i got bored so we decided on a multi-base functionality
 def toStr(n,base):
-   convertString = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-   if n < base:
+    """
+    :param n: the number in decimal base
+    :param base: the wanted base
+    :return: a string representing the desired number in the chosen base
+
+    """
+    convertString = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if n < base:
       return convertString[n]
-   else:
-      return toStr(n//base,base) + convertString[n%base]
+    else:
+        return toStr(n//base,base) + convertString[n%base]
 while True:
     grid=np.zeros((1000,1000,3),dtype=np.uint8)
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -18,7 +24,7 @@ while True:
     if key in range(48,48+base) and key<58:#48-58 are the possible numbers
         number *= base
         number += key-48
-    if key in range(97,97+base-10) and base > 9:
+    if key in range(97,97+base-10) and base > 9: #every character from A-Z if the base allows
         number*=base
         number+=key-87
     if key is 13: #13 is Enter
