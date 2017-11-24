ac6679ab4d6d1912061bc62748f76107958e6780
pauloalbert <student@Robotics.k12.il>
2017-11-24 15:46:36 +0200

    Finished turnToTarget command
    Added the ability to constantly read the cameras values to correct
    errors

--- src/org/usfirst/frc/team5987/robot/commands/TurnToTargetCommand.java
+++ src/org/usfirst/frc/team5987/robot/commands/TurnToTargetCommand.java
@@ -9,23 +9,31 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- *
+ * 
  */
 public class TurnToTargetCommand extends Command {
 	DrivingSubsystem driveSubsystem;
+	Timer timer = new Timer();
+	double lastTime;
+	
 	// these values you get from the raspberry pi.
-	double InitialTargetAngle = 0;
-	double InitialTargetDistance = 0;
+	double prevCameraTargetAngle = 0;
+	double cameraTargetAngle = 0;
+	double cameraTargetDistance = 0;
 
 	// These are determined at the start of the command
 	double ConstantP;
 	double ConstantI;
 	double ConstantD;
-	double delay = 0.005;
+	
+	double DELAY = 0.005;
 
 	// the angle from the center of the robot to the target
 	double robotTargetAngle;
+	
+	//error: the angle the robot needs to turn
 	double error, prevError;
+	//pid changing values
 	double P, I, D;
 	double Si = 0;
 
@@ -38,32 +46,48 @@ public class TurnToTargetCommand extends Command {
 	// Called just before this Command runs the first time
 	protected void initialize() {
 		Robot.driveSubsystem.resetNavX();
+		timer.reset();
+		
+		//Getting the constants from smartdashboard.
 		ConstantP = SmartDashboard.getNumber("Pid: p - ", RobotMap.ConstantP);
 		ConstantI = SmartDashboard.getNumber("Pid: i - ", RobotMap.ConstantI);
 		ConstantD = SmartDashboard.getNumber("Pid: d - ", RobotMap.ConstantD);
-		InitialTargetAngle = SmartDashboard.getNumber("Target Angle: ", 90);
-		InitialTargetDistance = SmartDashboard.getNumber("Target Distance: ", 50);
-		robotTargetAngle = Math.toDegrees(Math.atan((InitialTargetDistance * Math.sin(InitialTargetAngle))
-				- (InitialTargetDistance * Math.cos(InitialTargetAngle) + RobotMap.distanceFromCenter)));
-
-		prevError = InitialTargetAngle - Robot.driveSubsystem.getAngle();
+		
+		//getting the CAMERA VALUES from the raspberry pi
+		prevCameraTargetAngle = SmartDashboard.getNumber("Target Angle: ", 30);
+		cameraTargetAngle = SmartDashboard.getNumber("Target Angle: ", 30);
+		cameraTargetDistance = SmartDashboard.getNumber("Target Distance: ", 50);
+		
+		//Angle from the robot base
+		robotTargetAngle = cameraToCenterAngle(cameraTargetAngle, cameraTargetDistance);
+		
+		prevError = robotTargetAngle - Robot.driveSubsystem.getAngle();
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
+		//Check if the camera Values changed, and fix the target.
+		cameraTargetAngle = SmartDashboard.getNumber("Target Angle: ", 30);
+		if(cameraTargetAngle != prevCameraTargetAngle){
+			robotTargetAngle = cameraToCenterAngle(cameraTargetAngle, cameraTargetDistance)
+					+ Robot.driveSubsystem.getAngle();
+		}
+		double timeDiff = timer.get() - lastTime;
+		lastTime = timer.get();
 		
-		error = InitialTargetAngle - Robot.driveSubsystem.getAngle();
+		error = robotTargetAngle - Robot.driveSubsystem.getAngle();
 		SmartDashboard.putNumber("Error", error);
 		SmartDashboard.putNumber("Angle", Robot.driveSubsystem.getAngle());
 		P = ConstantP * error;
-		Si += delay * (prevError + error) / 2;
+		Si += timeDiff * (prevError + error) / 2;
 		I = ConstantI * Si;
-		D = ConstantD * (error - prevError) / delay;
+		D = ConstantD * (error - prevError) / timeDiff;
 
 		SmartDashboard.putNumber("Val motors", (P + I + D));
 		Robot.driveSubsystem.drive(P + I + D, -(P + I + D));
 		prevError = error;
-		Timer.delay(delay);
+		Timer.delay(DELAY);
+		prevCameraTargetAngle = cameraTargetAngle;
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
@@ -79,4 +103,22 @@ public class TurnToTargetCommand extends Command {
 	// subsystems is scheduled to run
 	protected void interrupted() {
 	}
+	
+	/**
+	 * Turns the camera angle into the angle of the center of the robot.
+	 * @param camAngle Camera angle in degrees
+	 * @param camDistance Camera distance in degrees
+	 * @return Robot center angle
+	 */
+	private double cameraToCenterAngle(double camAngle, double camDistance){
+		camAngle = Math.toRadians(camAngle);
+		return Math.toDegrees(
+				Math.atan(
+				(camDistance * Math.sin(camAngle))
+				/ (camDistance * Math.cos(camAngle) + RobotMap.distanceFromCenter))
+			);
+	}
 }
+
+//Find more accurate delay
+//Document
