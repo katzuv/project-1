9fdf583fb21c126ad49108fafc3d6b3784cb0d61
pauloalbert <student@Robotics.k12.il>
2017-11-26 17:20:55 +0200

    Added Joystick and subsystem classes

---
+++ src/org/usfirst/frc/team5987/robot/commands/JoystickDriveCommand.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team5987.robot.commands;
+
+import org.usfirst.frc.team5987.robot.OI;
+import org.usfirst.frc.team5987.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class JoystickDriveCommand extends Command {
+
+    public JoystickDriveCommand() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.driveSubsystem);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.driveSubsystem.drive(Robot.oi.leftStick.getY(), Robot.oi.rightStick.getY());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
